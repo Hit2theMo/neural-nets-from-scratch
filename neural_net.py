6148711f581285fa82018e5b5f842df3502c2b81
Mohit Khanwale <mohit.khanwale@gmail.com>
2020-05-02 17:45:59 +0530

    Dot product using numpy without loops

--- neural_net.py
+++ neural_net.py
@@ -5,20 +5,6 @@ weights = [[0.2, 0.8, -0.5, 1.0],
            [-0.26, -0.27, 0.17, 0.87]]
 biases = [2.0, 3.0, 0.5]
 
-layer_op = []
-for i in range(len(weights)):
-    dot = np.dot(weights[i], inputs)+biases[i]
-    layer_op.append(dot)
-print(layer_op)
-
-'''
-#Using ZIP
-layer_op = []
-for i in range(len(weights)):
-    nn = 0
-    for a, b in zip(inputs, weights[i]):
-        nn += (a * b)
-    layer_op.append(nn+biases[i])
-print(layer_op)
-'''
+output = np.dot(weights, inputs)+biases
+print(output)
 
