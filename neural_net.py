d4f044bea199db8561ddb0cc0b80a7761ec293a3
Mohit Khanwale <mohit.khanwale@gmail.com>
2020-05-02 20:01:44 +0530

    Added class to create layer and do a forward pass

--- neural_net.py
+++ neural_net.py
@@ -1,14 +1,22 @@
 import numpy as np
-#shape=(3,4)
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
-#shape=(3,4)
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
 
-biases = [2.0, 3.0, 0.5]
+np.random.seed(0)
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-output = np.dot(inputs, np.array(weights).T) + biases
-print(output)
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = Layer_Dense(4, 10)
+layer2 = Layer_Dense(10, 2)
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
