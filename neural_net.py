14afa1c3b98b76848d073a44c2188647643edc38
Mohit Khanwale <mohit.khanwale@gmail.com>
2020-05-02 18:38:16 +0530

    Added Batch Input

--- neural_net.py
+++ neural_net.py
@@ -1,10 +1,14 @@
 import numpy as np
-inputs = [1.0, 2.0, 3.0, 2.5]
+#shape=(3,4)
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+#shape=(3,4)
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
+
 biases = [2.0, 3.0, 0.5]
 
-output = np.dot(weights, inputs)+biases
+output = np.dot(inputs, np.array(weights).T) + biases
 print(output)
-
